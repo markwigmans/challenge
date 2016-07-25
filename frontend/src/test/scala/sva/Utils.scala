2ee7b91efc2df95c7a76d809a6f51c6532e50c49
Mark Wigmans <mark.wigmans@ximedes.com>
2016-07-25 23:51:00 +0200

    add licence text to files

--- frontend/src/test/scala/sva/Utils.scala
+++ frontend/src/test/scala/sva/Utils.scala
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2016 Mark Wigmans (mark.wigmans@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package sva
 
 import scala.util.Random
@@ -7,7 +22,7 @@ import scala.util.Random
   */
 object Utils {
 
-  // set the seed so the result is reproducable
+  // set the seed so the result is reproducible
   private val RNG = new Random(1234)
 
   def randInt(a:Int) = RNG.nextInt(a)
